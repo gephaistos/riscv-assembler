6597c238a1ece0f8d0dce50199ee17f023a6ce03
gephaistos <olehmaksa@gmail.com>
2023-03-08 16:22:39 +0200

    Apply `black` formatter

--- riscv_assembler/prjtest.py
+++ riscv_assembler/prjtest.py
@@ -1,11 +1,11 @@
 from project_convert import *
 from convert import *
 
-#pc = ProjectConverter('tests/assembly')
+# pc = ProjectConverter('tests/assembly')
 
-pc = ProjectConverter(root = 'tests/assembly/prjtest1')
+pc = ProjectConverter(root="tests/assembly/prjtest1")
 
-pc.setOutputType('r')
+pc.setOutputType("r")
 pc.setHex(True)
 
 res = pc.convert()
@@ -14,7 +14,6 @@ print("\n".join(["{}: {}".format(p, len(res[p])) for p in res.keys()]))
 print(pc.getFailedConvert())
 
 
-
-#TEST CASES
+# TEST CASES
 # 1. w/ or w/o filepath
 # 2. incorrect filepath raises error
